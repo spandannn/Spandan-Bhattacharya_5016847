b4a326283c44e3032d97c57e6fce4553ece445d2
Spandan Bhattacharya <164770715+spandannn@users.noreply.github.com>
2024-08-21 08:57:52 +0530

    Add files via upload

---
+++ Week 4,5/Spring REST/Exercise 4: Online Bookstore - Processing Request Body and Form Data/BookstoreAPI/src/test/java/com/example/bookstoreapi/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.bookstoreapi.repository;
+
+import com.example.bookstoreapi.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContaining(String title);
+    List<Book> findByAuthorContaining(String author);
+    List<Book> findByTitleContainingAndAuthorContaining(String title, String author);
+}
