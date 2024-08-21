8a4e40299f6031e094dfd8cc424cf04fe5c9f3e4
Spandan Bhattacharya <164770715+spandannn@users.noreply.github.com>
2024-08-21 08:52:43 +0530

    Add files via upload

---
+++ Week 4,5/Spring REST/Exercise 3: Online Bookstore - Handling Path Variables and Query Parameters/BookstoreAPI/src/test/java/com/example/bookstoreapi/entity/Book.java
@@ -0,0 +1,24 @@
+package com.example.bookstoreapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+}
